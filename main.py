bb524738f802fd126e94f8d7c5a4e748461930f7
Jeff Tagle <jeffreyrftagle@gmail.com>
2022-09-28 16:13:36 -0400

    added two TODOs

--- main.py
+++ main.py
@@ -3,3 +3,7 @@
 # TODO #2: Have a way to turn "off" coffee machine; should end code with execution
 
 # TODO #3: have Report prompt that shows current resources
+
+# TODO #4: Check if resources are sufficient.
+
+# TODO #5: Process coins.
