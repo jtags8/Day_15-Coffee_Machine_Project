a3f83036e12b84dcb8264d7a1cab511108f9655d
Jeff Tagle <jeffreyrftagle@gmail.com>
2022-09-28 16:09:51 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# TODO #1: prompt user by asking "What would you like? espresso/latte/cappuccino"
+
+# TODO #2: Have a way to turn "off" coffee machine; should end code with execution
+
+# TODO #3: have Report prompt that shows current resources
