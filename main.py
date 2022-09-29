62cb9511eaea6893a85f4d7d0bc8212c48ff9c21
Jeff Tagle <jeffreyrftagle@gmail.com>
2022-09-29 13:12:24 -0400

    TODO list done

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 # TODO #1: prompt user by asking "What would you like? espresso/latte/cappuccino"
+#   Use user's input to decide next st ep.
+#   Prompt should show again after each completed action to serve next customer.
 
-# TODO #2: Have a way to turn "off" coffee machine; should end code with execution
+# TODO #2: Have a way to turn "off" coffee machine; should end code with execution.
 
-# TODO #3: have Report prompt that shows current resources
+# TODO #3: have Report prompt that shows current resources.
+#   Water: ___ml
+#   Milk: __ml
+#   Coffee: __g
+#   Money: $__
 
 # TODO #4: Check if resources are sufficient.
+#   IF not enough, do not make drink, instead print "Sorry there is not enough ___."
 
 # TODO #5: Process coins.
+#   Need to calculate monetary value of coins inserted.
+#   Prompt user to input coins used.
 
-# TODO #6:
+# TODO #6: Need to check if transaction is successful
+#   Enough money entered? If not, should say "Sorry, not enough money. Money refunded."
+#   Else, cost of drink gets added to machine as profit and reflect in report.
+#   If too much money, need to give change. "Here is ___ in change." rounded to 2 decimals.
+
+# TODO #7: Make coffee.
+#   Deduct resources from machine resources.
+#   Tell user "Here is your___. Enjoy!"
